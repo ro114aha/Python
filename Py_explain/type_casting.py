f84081899801b6cd84836daf88f1b0bf759f8dd9
Roelf <ao617356@achmea.nl>
2024-12-10 14:03:25 +0100

    add example to demonstrate handling value errors in type casting and counting name length in Python

--- Py_explain/type_casting.py
+++ Py_explain/type_casting.py
@@ -1,5 +1,15 @@
 # Type converse
 
-int("123")
-print(int("123"))
-print(type(int("123")))
+# int("123")
+# print(int("123"))
+# print(type(int("123")))
+
+# Value error example
+# int("abc")
+# print(int("abc"))
+
+# print("Number of letters in your name: "+ len(input("Enter your name")))
+name=(input("Enter your name? =>"))
+lenght_name=len(name)
+print("lenght_name")
+print("Numbers of letters in your name: " + str(lenght_name)) 
