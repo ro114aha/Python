3004af4ebedbe52f5010b2750b65ec1f4ebed2a4
Roelf <ao617356@achmea.nl>
2024-12-11 20:27:07 +0100

    add examples of mathematical operations and order of operations in Python

---
+++ Py_explain/math_in.py
@@ -0,0 +1,26 @@
+# # mathematic operations in python
+
+# # PEMDAS = order of calculation
+
+# # Parentheses ()
+# print(2*(4+6))
+# print((2*4)+6)
+
+# # Exponents
+# print(2**2)
+
+# # Multiply and Division equal
+# print(2*2/4)
+# print(2/4*2)
+
+# # Addition/Subtraction
+# print((2+2)-(2+2))
+
+# # Specials
+# print(2/2)
+# print(type(2/2))
+# print(2//2)
+# print(type(2//2)) # remove everything behind the comma
+
+# debug to understand the order
+print(3*(3 + 3)/3 -3)
