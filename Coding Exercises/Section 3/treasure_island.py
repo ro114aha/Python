84f9281f2e8eae329eaf9c34facb2b3839105f41
Roelf <ao617356@achmea.nl>
2025-01-23 17:31:15 +0100

    fix: enhance input handling for direction choice in treasure island game

--- Coding Exercises/Section 3/treasure_island.py
+++ Coding Exercises/Section 3/treasure_island.py
@@ -20,7 +20,7 @@ print('''
 Your mission is to escape from Dragon Island
 ''')
 choise_1 =input("Do you want to go left or right? => ")
-if choise_1 == "left" or choise_1 == "Left":
+if choise_1 == "left" or choise_1 == "Left" or choise_1 == "LEFT":
     print('''
 *******************************************************************************
                  __.-/|
@@ -40,4 +40,4 @@ if choise_1 == "left" or choise_1 == "Left":
                          EATEN BY THE DRAGON 
 ''')
 else:
-    print("Good choise your still alive!")
+    print('Good choise your still alive!:"Good to know".')
