3bca21962a6ef41d9f43210822ca285e260b6caa
Roelf <ao617356@achmea.nl>
2025-01-27 22:44:22 +0100

    add: create flowchart for heads or tails game and explain list operations in Python

---
+++ Py_explain/list.py
@@ -0,0 +1,10 @@
+# list explained
+
+fruits= ["apple", "peer"]
+print(fruits[1])
+#append an item to the list
+fruits.append("sinnasappel")
+print(fruits)
+#extend
+fruits.extend(["banana", "kiwi", "grape"])
+print(fruits)
