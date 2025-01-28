cab38ff5dfb03692800df062c33acb3a8575eb3d
Roelf <ao617356@achmea.nl>
2025-01-28 22:01:29 +0100

    add: implement bankers roulette game with friends list

---
+++ Coding Exercises/Section 4/bankers_roulette.py
@@ -0,0 +1,5 @@
+# bankers roulette
+# In the city (London) people do something special when thy have to pay the bill
+# the put their creditcards into a bowl. And the one who is picket must pay for the group
+friends = ["Mike", "Sjors", "Coby", "Laurien", "Bill"]
+print(friends)
