4a7ba5a2da4d6de8ade9d3b4df3969be15660e58
Roelf <ao617356@achmea.nl>
2025-01-28 22:38:29 +0100

    add: print specific vegetable from nested list in list.py

--- Py_explain/list.py
+++ Py_explain/list.py
@@ -24,3 +24,12 @@ dirty_dozen = [fruits, vegetables]
 
 print(dirty_dozen)
 
+# what will be print
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinach", "Kale", "Tomatoes", "Celery", "Potatoes"]
+ 
+dirty_dozen = [fruits, vegetables]
+ 
+print(dirty_dozen[1][1])
+
