087459a241abd307ffcd35618e880471b87c4929
Roelf <ao617356@achmea.nl>
2025-01-28 22:11:39 +0100

    add: select random friend to pay in bankers roulette game

--- Coding Exercises/Section 4/bankers_roulette.py
+++ Coding Exercises/Section 4/bankers_roulette.py
@@ -1,5 +1,9 @@
 # bankers roulette
 # In the city (London) people do something special when thy have to pay the bill
 # the put their creditcards into a bowl. And the one who is picket must pay for the group
+import random
+
+# Option 1
 friends = ["Mike", "Sjors", "Coby", "Laurien", "Bill"]
-print(friends)
+who_pays = random.choice(friends)   
+print(who_pays)
