db93809c6bd31d24ffd43e18047686ca60d5edc8
Roelf <ao617356@achmea.nl>
2025-01-31 17:51:47 +0100

    add: create py_password_generator.py for a simple password generation tool

---
+++ Coding Exercises/Section 5/py_password_generator.py
@@ -0,0 +1,22 @@
+import string
+
+# Create a simple password generator
+#LETTERS
+# Step 1 make a list of letters
+letters = list(string.ascii_letters)
+print(letters)
+#Step 2 Ask user how many letters the password should contain
+how_many_letters =
+
+
+#NUMBERS
+# Step 1 make a list of numbers
+numbers = list(range(1, 12))
+print(numbers)
+
+#SYMBOLS
+# Step 1 make a list of symbols
+symbols = list("!@#$%^&*()-_=+[]{}?/")
+print(symbols)
+
+
